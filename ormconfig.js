5d3c1e9b44aabcf1e684988f09393bb968dd7436
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-08-30 11:34:41 -0300

    Adicionada as configurações do Heroku

---
+++ ormconfig.js
@@ -0,0 +1,11 @@
+require("dotenv/config");
+
+module.exports = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  entities: [process.env.ENTITIES],
+  migrations: [process.env.MIGRATIONS],
+  cli: {
+    migrationsDir: "./src/database/migrations",
+  },
+};
